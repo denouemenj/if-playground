6ab105b4af5907e5347404d0b6a569d95f2abd1e
jiayuylinfs <jiayu.li.nfs@gmail.com>
2017-07-14 20:14:49 -0300

    v1

---
+++ if-playground/if-playground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  if-playground
+//
+//  Created by Jiayu li on 2017-07-14.
+//  Copyright © 2017 Jiayu li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
